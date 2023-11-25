1843ea68af8f9ec6af7750a51a37d359819c6b95
wandaaa <isnainiswandaaa.gmail.com>
2023-11-25 15:14:01 +0700

    Menambahkan template

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProdiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,12 +37,41 @@ Route::get('/dpa', function () {
 });
 
 
-// ------------------------------------ Kaprodi
-Route::get('/kaprodi', function () {
-    return view('Kaprodi/home',[
-        "title" => "Kaprodi"
-    ]);
-});
+// ------------------------------------ Prodi
+Route::get('/prodi/home', [ProdiController::class, 'index']);
+Route::get('/prodi/users-profile', [ProdiController::class, 'usersProfile']);
+Route::get('/prodi/pages-faq', [ProdiController::class, 'pagesFaq']);
+Route::get('/prodi/pages-contact', [ProdiController::class, 'pagesContact']);
+Route::get('/prodi/pages-register', [ProdiController::class, 'pagesRegister']);
+Route::get('/prodi/pages-login', [ProdiController::class, 'pagesLogin']);
+Route::get('/prodi/pages-error-404', [ProdiController::class, 'pagesError404']);
+Route::get('/prodi/pages-blank', [ProdiController::class, 'pagesBlank']);
+Route::get('/prodi/components-alerts', [ProdiController::class, 'componentsAlerts']);
+Route::get('/prodi/components-accordion', [ProdiController::class, 'componentsAccordion']);
+Route::get('/prodi/components-badges', [ProdiController::class, 'componentsBadges']);
+Route::get('/prodi/components-breadcrumbs', [ProdiController::class, 'componentsBreadcrumbs']);
+Route::get('/prodi/components-buttons', [ProdiController::class, 'componentsButtons']);
+Route::get('/prodi/components-cards', [ProdiController::class, 'componentsCards']);
+Route::get('/prodi/components-carousel', [ProdiController::class, 'componentsCarousel']);
+Route::get('/prodi/components-list-group', [ProdiController::class, 'componentsListGroup']);
+Route::get('/prodi/components-modal', [ProdiController::class, 'componentsModal']);
+Route::get('/prodi/components-tabs', [ProdiController::class, 'componentsTabs']);
+Route::get('/prodi/components-pagination', [ProdiController::class, 'componentsPagination']);
+Route::get('/prodi/components-progress', [ProdiController::class, 'componentsProgress']);
+Route::get('/prodi/components-spinners', [ProdiController::class, 'componentsSpinners']);
+Route::get('/prodi/components-tooltips', [ProdiController::class, 'componentsTooltips']);
+Route::get('/prodi/forms-elements', [ProdiController::class, 'formsElements']);
+Route::get('/prodi/forms-layouts', [ProdiController::class, 'formsLayouts']);
+Route::get('/prodi/forms-editors', [ProdiController::class, 'formsEditors']);
+Route::get('/prodi/forms-validation', [ProdiController::class, 'formsValidation']);
+Route::get('/prodi/tables-general', [ProdiController::class, 'tablesGeneral']);
+Route::get('/prodi/tables-data', [ProdiController::class, 'tablesData']);
+Route::get('/prodi/charts-chartjs', [ProdiController::class, 'chartsChartjs']);
+Route::get('/prodi/charts-apexcharts', [ProdiController::class, 'chartsApexcharts']);
+Route::get('/prodi/charts-echarts', [ProdiController::class, 'chartsEcharts']);
+Route::get('/prodi/icons-bootstrap', [ProdiController::class, 'iconsBootstrap']);
+Route::get('/prodi/icons-remix', [ProdiController::class, 'iconsRemix']);
+Route::get('/prodi/icons-boxicons', [ProdiController::class, 'iconsBoxicons']);
 
 
 // ------------------------------------ Fakultas
