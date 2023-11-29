6667dd59ec774519f515d257bd4859e0b4c2516a
wandaaa <isnainiswandaaa.gmail.com>
2023-11-29 14:05:15 +0700

    resolve conflict

--- app/Models/Prodi.php
+++ app/Models/Prodi.php
@@ -1,15 +1,1 @@
-<?php
 
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-
-class Prodi extends Model
-{
-    use HasFactory;
-
-    protected $table = 'prodi';
-    protected $guards = ['kodeProdi'];
-}
